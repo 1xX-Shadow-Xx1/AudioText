c1bda399195bfbd6411abd9d6cf5625fe120d00a
1xX-Shadow-Xx1 <kevinliriano200816@gmail.com>
2025-11-27 18:22:30 -0400

    Basse

--- obj/Debug/net9.0/AudioText.AssemblyInfo.cs
+++ obj/Debug/net9.0/AudioText.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AudioText")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89ffd5321ece28bffc8dcf684fe179694700d612")]
 [assembly: System.Reflection.AssemblyProductAttribute("AudioText")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AudioText")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
